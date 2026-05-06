88d67c517a9710c2a6f4ccf3177d9a72b7d546c4
Adalberto Pompolo <beto.nassu@gmail.com>
2026-05-05 23:21:54 -0300

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -4,10 +4,28 @@ import * as net from "net";
 console.log("Logs from your program will appear here!");
 
 const server = net.createServer((socket) => {
-  socket.write("HTTP/1.1 200 OK\r\n\r\n");
+  socket.write(createResponse(200));
+  socket.on('data', (chunk) => {
+    const str = chunk.toString();
+    const [status] = str.split("\n");
+    const [_method, url] = status.split(" ");
+
+    if (url === '/') {
+      socket.write(createResponse(200));
+    } else {
+      socket.write(createResponse(404));
+    }
+  })
   socket.on("close", () => {
     socket.end();
   });
 });
 
 server.listen(4221, "localhost");
+
+const crlf = "\r\n" as const;
+
+function createResponse(status: 404 | 200): string {
+  const statusMessage = status === 404 ? "Not Found" : "OK";
+  return `HTTP/1.1 ${status} ${statusMessage}${crlf}${crlf}`;
+}
