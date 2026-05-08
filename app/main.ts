fd7f2ab03fdfc233a12ce6b2cec97ea598a248a1
Adalberto Pompolo <beto.nassu@gmail.com>
2026-05-07 22:14:10 -0300

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,7 +1,10 @@
 import * as net from "net";
 
+const fileDirFlagIndex = Bun.argv.findIndex(flag => flag === "--directory");
+const fileDir = fileDirFlagIndex !== -1 ? Bun.argv[fileDirFlagIndex + 1] : "";
+
 const server = net.createServer((socket) => {
-  socket.on('data', (chunk) => {
+  socket.on('data', async (chunk) => {
     const {status, headers} = deserialize(chunk.toString());
     const {url} = deserializeStatusRequest(status);
 
@@ -20,7 +23,7 @@ const server = net.createServer((socket) => {
           body
         )
       );
-    } else if(url.startsWith('/user-agent')) {
+    } else if (url.startsWith('/user-agent')) {
       const userAgentValue = headers['User-Agent'];
       socket.write(
         createResponse(
@@ -32,6 +35,24 @@ const server = net.createServer((socket) => {
           userAgentValue
         )
       )
+    } else if (url.startsWith('/files')) {
+      try {
+        const filename = url.replace('/files/', '');
+        const fileURL = Bun.pathToFileURL(`${fileDir}/${filename}`);
+        const file = await Bun.file(fileURL).text();
+        socket.write(
+          createResponse(
+            200,
+            serializeHeaders({
+              'Content-Type': 'application/octet-stream',
+              'Content-Length': file.length.toString(),
+            }),
+            file
+          )
+        );
+      } catch (e) {
+        socket.write(createResponse(404));
+      }
     }
     else {
       socket.write(createResponse(404));
